e4c4db80f1bd9611ca9864d64c40f055a4cd14dd
ConanGuy <mathieuhuin@free.fr>
2024-06-19 05:13:57 +0200

    Updated filtering method + Added manual parameters to swagger

--- api/views/offer_group.py
+++ api/views/offer_group.py
@@ -1,9 +1,10 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, response
+from rest_framework import generics, response, status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .offer import OfferFilter, OfferListCreate
-from ..models import OfferGroup
-from ..serializers import OfferGroupSerializer
+from ..models import OfferGroup, Offer
+from ..serializers import OfferGroupSerializer, OfferSerializer
 
 class OfferGroupList(generics.ListAPIView):
     queryset = OfferGroup.objects.all()
@@ -18,16 +19,24 @@ class OfferGroupRetrieve(generics.RetrieveAPIView):
     serializer_class = OfferGroupSerializer
         
 class OfferGroupRetrieveWithOffers(generics.RetrieveAPIView):
-    queryset = OfferGroup.objects.all()
+    queryset = OfferGroup.objects.all().prefetch_related('offers')
     serializer_class = OfferGroupSerializer
 
-    def get(self, request, *args, **kwargs):        
+    def get_filtered_offers(self, offer_group, request):
+        # Retrieve filters from the request query params
+        filters = OfferFilter(data=request.query_params, queryset=Offer.objects.filter(offer_group=offer_group))
+        return filters.qs
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('lu', openapi.IN_QUERY, description="Filter by lu", type=openapi.TYPE_BOOLEAN),
+        openapi.Parameter('order', openapi.IN_QUERY, description="Order by date or site", type=openapi.TYPE_STRING, enum=['date', '-date', 'site', '-site']),
+    ])
+    def get(self, request, *args, **kwargs):
         offer_group = self.get_object()
         offer_group_data = OfferGroupSerializer(offer_group).data
         
-        offers = OfferListCreate.as_view()(request._request).data
-        offers = [offer for offer in offers if offer['offer_group']['id'] == offer_group.id]
+        offers = self.get_filtered_offers(offer_group, request)
         
-        offer_group_data['offers'] = offers
-        
-        return response.Response(offer_group_data)
+        offer_group_data['offers'] = OfferSerializer(offers, many=True).data
+
+        return response.Response(offer_group_data, status=status.HTTP_200_OK)
